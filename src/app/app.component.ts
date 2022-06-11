c9e0865452a9f253a18a2decb91b5d9611b2550e
yochi66865 <yochi66865@gmail.com>
2022-06-11 22:09:33 +0300

    delete modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { ModalComponent } from './modal/modal.component';
 
 @Component({
   selector: 'app-root',
@@ -13,16 +12,4 @@ export class AppComponent {
   animal = 'dog';
   constructor(private dialog: MatDialog) { }
   title = 'basic-angular';
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open(ModalComponent, {
-      width: '250px',
-      data: { name: this.name, animal: this.animal }
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      this.animal = result;
-    });
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { BrowserModule } from '@angular/platform-browser';
@@ -9,17 +8,14 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ModalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatDialogModule,
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
